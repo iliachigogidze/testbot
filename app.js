df530eed139a6933bdbc247946a65d60f9a26ffa
Ilia Chigogidze <iliachigogidze@gmail.com>
2018-03-14 15:03:53 +0400

    sdsd

--- app.js
+++ app.js
@@ -33,7 +33,34 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("Hi... We sell shirts. Say 'show shirts' to see our products.");
+});
+
+// Add dialog to return list of shirts available
+bot.dialog('showShirts', function (session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+            .title("Classic White T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+            ]),
+        new builder.HeroCard(session)
+            .title("Classic Gray T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+            ])
+    ]);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: /^(show|list)/i });
 bot.set('storage', tableStorage);
 
 // Make sure you add code to validate these fields
@@ -57,29 +84,6 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 })
 .matches('change/return-ticket', (session) => {
     session.send('ბილეთის გადაცვლა არ მოსულა', session.message.text);
-    bot.dialog('showShirts', function (session) {
-        var msg = new builder.Message(session);
-        msg.attachmentLayout(builder.AttachmentLayout.carousel)
-        msg.attachments([
-            new builder.HeroCard(session)
-                .title("Classic White T-Shirt")
-                .subtitle("100% Soft and Luxurious Cotton")
-                .text("Price is $25 and carried in sizes (S, M, L, and XL)")
-                .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
-                .buttons([
-                    builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
-                ]),
-            new builder.HeroCard(session)
-                .title("Classic Gray T-Shirt")
-                .subtitle("100% Soft and Luxurious Cotton")
-                .text("Price is $25 and carried in sizes (S, M, L, and XL)")
-                .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
-                .buttons([
-                    builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
-                ])
-        ]);
-        session.send(msg).endDialog();
-    })
 })
 .matches('children', (session) => {
     session.send('ბავშვებისთვის 6 წლიდან ბილეთის ყიდვა აუცილებელია', session.message.text);
