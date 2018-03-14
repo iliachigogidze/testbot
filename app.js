37edf0193cfa28e9800c30eb82ec6ec24f9b745c
Ilia Chigogidze <iliachigogidze@gmail.com>
2018-03-14 14:22:39 +0400

    sdsd

--- app.js
+++ app.js
@@ -57,6 +57,29 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 })
 .matches('change/return-ticket', (session) => {
     session.send('ბილეთის გადაცვლა არ მოსულა', session.message.text);
+    bot.dialog('showShirts', function (session) {
+        var msg = new builder.Message(session);
+        msg.attachmentLayout(builder.AttachmentLayout.carousel)
+        msg.attachments([
+            new builder.HeroCard(session)
+                .title("Classic White T-Shirt")
+                .subtitle("100% Soft and Luxurious Cotton")
+                .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+                .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+                .buttons([
+                    builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+                ]),
+            new builder.HeroCard(session)
+                .title("Classic Gray T-Shirt")
+                .subtitle("100% Soft and Luxurious Cotton")
+                .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+                .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+                .buttons([
+                    builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+                ])
+        ]);
+        session.send(msg).endDialog();
+    })
 })
 .matches('children', (session) => {
     session.send('ბავშვებისთვის 6 წლიდან ბილეთის ყიდვა აუცილებელია', session.message.text);
